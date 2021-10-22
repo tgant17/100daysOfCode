18d97c5c10238a97a9faf10c1a5cc57746f27833
tgant17 <44172930+tgant17@users.noreply.github.com>
2021-10-22 01:40:22 -0500

    played around with react native

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>where they at ?</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={
+          [{key: "tristan"}, {key: "maelyn"}, {key: "hanna"}, {key: "where the bitches at"}
+          ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
